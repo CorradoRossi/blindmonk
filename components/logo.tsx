c18f3bfdfe4a1d430269b6d818f644e283513ee1
Rossi Meacham <rossi@frugaltricks.com>
2021-06-04 15:26:56 -0400

    hmm

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,14 +1,26 @@
+import { useState, useEffect } from 'react';
 import styles from './logo.module.css';
 import { SITE_NAME_MULTILINE } from '@lib/constants';
 import { getLogoImg } from '@lib/cms-providers/dato';
 
-export default async function Logo({ textSecondaryColor = 'var(--accents-5)' }) {
+async function doit() {
   const logoImgs = await getLogoImg();
-  const logoImg = logoImgs[0];
+  const logoImg = logoImgs.map((log: any) => {
+    return log.logo;
+  });
+  return logoImg[0];
+}
+
+export default function Logo({ textSecondaryColor = 'var(--accents-5)' }) {
+  const [logo, setLogo] = useState();
+
+  useEffect(() => {
+    doit().then((res: any) => setLogo(res));
+  }, []);
   return (
     <div className={styles.logo}>
       <div className={styles.icon}>
-        <img src={logoImg.logo} height="40" />
+        <img src={logo} height="40" />
       </div>
       <div className={styles.text}>
         <div>{SITE_NAME_MULTILINE[0]}</div>
