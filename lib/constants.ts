1851cd8406b01a86ff7a0ce89e8e9536eb69e16e
Rossi Meacham <rossi@frugaltricks.com>
2021-06-04 14:24:03 -0400

    update constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,11 +1,11 @@
-export const SITE_URL = 'https://demo.vercel.events';
+export const SITE_URL = 'https://blindmonk.club';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
 export const TWITTER_USER_NAME = 'metastash';
 export const BRAND_NAME = 'Blindmonk';
 export const SITE_NAME_MULTILINE = ['Blind', 'Monk'];
 export const SITE_NAME = 'Blindmonk';
-export const META_DESCRIPTION = 'This is an NFT experiment';
-export const SITE_DESCRIPTION = 'This is an experiment';
+export const META_DESCRIPTION = 'NFT collection and marketplace';
+export const SITE_DESCRIPTION = 'NFT collection and marketplace';
 export const DATE = 'August, 2021';
 export const SHORT_DATE = 'June 4 - 9:00am PST';
 export const FULL_DATE = 'June 4 9am Pacific Time (GMT-7)';
@@ -17,30 +17,29 @@ export const COOKIE = 'user-id';
 export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
 export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
 
-export const CODE_OF_CONDUCT =
-  'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
-export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
+export const CODE_OF_CONDUCT = 'https://metastash.xyz';
+export const REPO = 'https://github.com/CorradoRossi/blindmonk';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
     name: 'Profile',
-    route: '/stage/a'
-  },
-  {
-    name: 'Wallet',
-    route: '/schedule'
+    route: '/profile'
   },
   {
     name: 'Collection',
-    route: '/speakers'
+    route: '/collection'
   },
   {
     name: 'Market',
-    route: '/expo'
+    route: '/market'
+  },
+  {
+    name: 'Wallet',
+    route: '/wallet'
   },
   {
     name: 'Settings',
-    route: '/jobs'
+    route: '/settings'
   }
 ];
 
