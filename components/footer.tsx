d41d6f55b238ae74d8a7bdf9013c2f384b1e2cab
Rossi Meacham <rossi@frugaltricks.com>
2021-06-04 10:28:51 -0400

    send it

--- components/footer.tsx
+++ components/footer.tsx
@@ -22,7 +22,7 @@ import { COPYRIGHT_HOLDER, SITE_NAME, CODE_OF_CONDUCT, LEGAL_URL, REPO } from '@
 export function HostedByVercel() {
   return (
     <a
-      href="https://vercel.com"
+      href="https://metastash.xyz"
       className={cn(styles['footer-link'], styles['footer-logo'])}
       target="_blank"
       rel="noopener noreferrer"
@@ -37,36 +37,7 @@ export default function Footer() {
   return (
     <footer className={cn(styles.footer)}>
       <div className={styles['footer-legal']}>
-        <div className={styles['footer-hostedby']}>
-          {/*<HostedByVercel />*/}
-          <div className={styles['footer-separator']} />
-        </div>
-        <div className={styles['footer-copyright']}>
-          Copyright © {`${new Date().getFullYear()} `} {COPYRIGHT_HOLDER || `${SITE_NAME}.`} All
-          rights reserved.
-        </div>
         <div className={styles['footer-center-group']}>
-          <p className={styles['footer-paragraph']}>
-            <a
-              href={REPO}
-              className={styles['footer-link']}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Source Code
-            </a>
-          </p>
-          <div className={styles['footer-separator']} />
-          <p className={styles['footer-paragraph']}>
-            <a
-              href={CODE_OF_CONDUCT}
-              className={styles['footer-link']}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Code of Conduct
-            </a>
-          </p>
           {LEGAL_URL && (
             <>
               <div className={styles['footer-separator']} />
--- components/index.tsx
+++ components/index.tsx
@@ -51,7 +51,7 @@ export default function Conf({
             <>
               <Hero />
               <Form />
-              <LearnMore />
+              {/*<LearnMore />*/}
             </>
           ) : (
             <Ticket
