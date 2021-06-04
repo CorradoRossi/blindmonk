efa1487a0554610fa4b4eba6221d21adafff1aec
Rossi Meacham <rossi@frugaltricks.com>
2021-06-04 08:58:14 -0400

    update cms

--- components/page.tsx
+++ components/page.tsx
@@ -50,9 +50,9 @@ export default function Page({ meta, children, fullViewport = false }: Props) {
         <meta property="og:description" content={description} />
         <meta name="twitter:site" content={`@${TWITTER_USER_NAME}`} />
         <meta name="twitter:card" content={image ? 'summary_large_image' : 'summary'} />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/tri-circle-purple.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/tri-circle-purple.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/tri-circle-purple.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link
--- lib/constants.ts
+++ lib/constants.ts
@@ -16,17 +16,15 @@
 
 export const SITE_URL = 'https://demo.vercel.events';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
-export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
-export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
-export const SITE_DESCRIPTION =
-  'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
-export const SHORT_DATE = 'Oct 27 - 9:00am PST';
-export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
+export const TWITTER_USER_NAME = 'metastash';
+export const BRAND_NAME = 'Blind Monk';
+export const SITE_NAME_MULTILINE = ['Blind', 'Monk'];
+export const SITE_NAME = 'Blind Monk';
+export const META_DESCRIPTION = 'This is an NFT experiment';
+export const SITE_DESCRIPTION = 'This is an experiment';
+export const DATE = 'June 4, 2021';
+export const SHORT_DATE = 'June 4 - 9:00am PST';
+export const FULL_DATE = 'June 4 9am Pacific Time (GMT-7)';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
