a38003f5de6d3088f19ca9c16232bbb7965ab5a9
Rossi Meacham <rossi@frugaltricks.com>
2021-06-05 15:31:14 -0400

    env and config stuff

--- lib/cms-api.ts
+++ lib/cms-api.ts
@@ -1,11 +1,7 @@
 import { Job, Sponsor, Stage, Speaker } from '@lib/types';
 
-import * as strapiApi from './cms-providers/strapi';
-import * as agilityApi from './cms-providers/agility';
 import * as datoCmsApi from './cms-providers/dato';
-import * as contentfulApi from './cms-providers/contentful';
 import * as prismicApi from './cms-providers/prismic';
-import * as storyblokApi from './cms-providers/storyblok';
 
 let cmsApi: {
   getAllSpeakers: () => Promise<Speaker[]>;
@@ -16,20 +12,8 @@ let cmsApi: {
 
 if (process.env.DATOCMS_READ_ONLY_API_TOKEN) {
   cmsApi = datoCmsApi;
-} else if (process.env.CONTENTFUL_ACCESS_TOKEN && process.env.CONTENTFUL_SPACE_ID) {
-  cmsApi = contentfulApi;
-} else if (process.env.STORYBLOK_PREVIEW_TOKEN) {
-  cmsApi = storyblokApi;
 } else if (process.env.PRISMIC_REPO_ID) {
   cmsApi = prismicApi;
-} else if (
-  process.env.AGILITY_GUID &&
-  process.env.AGILITY_API_FETCH_KEY &&
-  process.env.AGILITY_API_PREVIEW_KEY
-) {
-  cmsApi = agilityApi;
-} else if (process.env.STRAPI_API_URL) {
-  cmsApi = strapiApi;
 } else {
   cmsApi = {
     getAllSpeakers: async () => [],
