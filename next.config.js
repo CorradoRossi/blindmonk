7d570f22505eb5a3fa8206e7cab45050eaf08e7b
Rossi Meacham <rossi@frugaltricks.com>
2021-06-06 16:05:04 -0400

    gitignore stuff

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-/*
 // This file sets a custom webpack configuration to use your Next.js app
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
@@ -20,6 +19,7 @@ const SentryWebpackPluginOptions = {
   // Additional config options for the Sentry Webpack plugin. Keep in mind that
   // the following options are set automatically, and overriding them is not
   // recommended:
+  silent: true
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
   // For all available options, see:
@@ -29,14 +29,13 @@ const SentryWebpackPluginOptions = {
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
 module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
-*/
 
-module.exports = {
-  images: {
-    domains: [
-      'www.datocms-assets.com',
-      'localhost' // For Strapi
-    ],
-    imageSizes: [24, 64, 300]
-  }
-};
+//module.exports = {
+//  images: {
+//    domains: [
+//      'www.datocms-assets.com',
+//      'localhost' // For Strapi
+//    ],
+//    imageSizes: [24, 64, 300]
+//  }
+//};
