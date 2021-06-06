919425d87e43dd66d6bbe7b52c97c07aa5b93a62
Rossi Meacham <rossi@frugaltricks.com>
2021-06-06 14:10:40 -0400

    hmm

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/*
 // This file sets a custom webpack configuration to use your Next.js app
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
@@ -28,3 +29,14 @@ const SentryWebpackPluginOptions = {
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
 module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
+*/
+
+module.exports = {
+  images: {
+    domains: [
+      'www.datocms-assets.com',
+      'localhost' // For Strapi
+    ],
+    imageSizes: [24, 64, 300]
+  }
+};
