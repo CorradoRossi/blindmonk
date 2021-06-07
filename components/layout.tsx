27ebfaee1b32ea2c6aaa71161ed600749dd6d60d
Rossi Meacham <rossi@frugaltricks.com>
2021-06-06 20:04:12 -0400

    remove extra css module call

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/router';
 import { SkipNavContent } from '@reach/skip-nav';
 import { NAVIGATION } from '@lib/constants';
 import styles from 'styles/layout.module.css';
-import formStyles from 'styles/form.module.css';
 import Logo from './icons/icon-logo';
 import MobileMenu from './mobile-menu';
 import Footer from './footer';
