c1e2525a40385bcb0be1eebe2da6491c7e3078a7
Rossi Meacham <rossi@frugaltricks.com>
2021-06-09 09:28:04 -0400

    and update css module naming

--- components/home-container.tsx
+++ components/home-container.tsx
@@ -1,4 +1,4 @@
-import styles from 'styles/conf-container.module.css';
+import styles from 'styles/home-container.module.css';
 
 export default function HomeContainer({ children }: { children: React.ReactNode }) {
   return <div className={styles.container}>{children}</div>;
