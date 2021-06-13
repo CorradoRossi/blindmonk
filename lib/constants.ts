4f54d0d76f2907134e69454e959b1398f8f558cc
Rossi Meacham <rossi@frugaltricks.com>
2021-06-13 04:59:05 -0400

    env related stuff

--- lib/constants.ts
+++ lib/constants.ts
@@ -42,6 +42,8 @@ export const OPENSEA_CONTRACT = 'asset_contract';
 export const OPENSEA_COLLECTIONS = 'collections';
 
 export const API_URL = 'https://graphql.datocms.com/';
+export const API_URL_DRAFT = 'https://graphql.datocms.com/preview';
+export const API_REST_ENDPOINT = 'https://site-api.datocms.com/';
 export const API_TOKEN_READ = '6e37e962aaf5d60e9e1b3ce1dac08d';
 export const API_TOKEN_FULL = '9a86934493e273cb21af51fb40aa2f';
 export const API_TOKEN = process.env.DATOCMS_FULL_API_TOKEN;
